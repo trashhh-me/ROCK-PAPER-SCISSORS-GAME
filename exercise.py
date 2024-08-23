27a810b503d19dbfaf20b3657b3e1eb113c84169
trashhh_me <arunsingh.austin@gmail.com>
2024-08-23 21:18:43 +0545

    Maximum of 3 numbers using max function

--- exercise.py
+++ exercise.py
@@ -2,16 +2,11 @@
 
 
 def find_max(num1, num2, num3):
-    if num1>num2 and num1>num3:
-        return num1
-    elif num2>num1 and num2>num3:
-        return num2
-    else:
-        return num3
+    return max(num1, num2, num3)
     
 def main():
     x,y,z = map(int, input("Enter 3 numbers").split())
-    print("the maximum from the three is", find_max(x,y,z))
+    print("the maximum from the three is", find_max(x,y,z ))
 
 if __name__ == "__main__":
     main()
