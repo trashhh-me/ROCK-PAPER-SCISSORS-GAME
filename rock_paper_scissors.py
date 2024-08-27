f7ed2504933a988df86358f3208023b0c94149db
trashhh_me <arunsingh.austin@gmail.com>
2024-08-28 01:23:39 +0545

    Improved Game

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,5 +1,12 @@
 def ask_users_choice():
-    choice = input("Enter your pick (Rock/Paper/Scissor): ")
+    while True:
+        choice = input("Enter your pick (Rock/Paper/Scissor): ")
+        choice = choice.upper()
+        if choice == "ROCK" or choice == "SCISSOR" or choice =="PAPER":
+            break
+        else:
+            print("Invalid Input, Try again")
+            continue
     return choice
     
 def computers_choice():
@@ -19,10 +26,61 @@ def result(x,y):
         return "HUMAN WINS"
     
 def main():
-    x = ask_users_choice()
-    y = computers_choice()
-    z = result(x,y)
-    print(f"Your choice is {x} and the machine's choice is {y}, thus {z}")
+    import time
+    import os
+    u = input("This is rock paper scissor game, can you beat the machine?? \n \r Type Y to Continue, N to Forfeit ")
+    if u == "Y" or u == "y":
+        while True:
+            os.system('cls')
+            print("**********WELCOME TO ROCK PAPER SCISSORS GAME**********\n")
+            print("-----5 Game in a row, most win wins-----\n\n")  
+            c=0
+            h=0
 
-if __name__ == "__main__":
-    main()
+            for i in range(5):
+                x = ask_users_choice()
+                y = computers_choice()
+                z = result(x,y)
+                if z == "COMPUTER WINS":
+                    c+=1
+                    print("COMPUTER WINS", end='', flush=True)
+                    time.sleep(2)
+                    print("\r" + " " * len("COMPUTER WINS") + "\r", end='', flush=True)
+
+                elif  z == "HUMAN WINS":
+                    h+=1
+                    print("HUMAN WINS", end='', flush=True)
+                    time.sleep(2)
+                    print("\r" + " " * len("HUMAN WINS") + "\r", end='', flush=True)
+
+                else:
+                    print("DRAW", end='', flush=True)
+                    time.sleep(2)
+                    print("\r" + " " * len("DRAW") + "\r", end='', flush=True)
+                    continue
+            if c<h:
+                print("\n\n------------------------------------------------------------------------------")
+                print(f"\nYour Score is {h} and Machine Score is {c}")
+                print("\nYOU WON THE MACHINE")
+                print("\n------------------------------------------------------------------------------")
+            elif c>h:
+                print("\n\n------------------------------------------------------------------------------")
+                print(f"\nYour Score is {h} and Machine Score is {c}")
+                print("\nTHE MACHINE WON")
+                print("\n------------------------------------------------------------------------------")
+            else:
+                print("\n\n------------------------------------------------------------------------------")
+                print(f"\nYour Score is {h} and Machine Score is {c}")
+                print("\nDRAW")
+                print("\n------------------------------------------------------------------------------")
+            con = input("\n\n DO YOU WANT TO CONTINUE (Y/N)")
+            con = con.upper()
+            if con == "Y":
+                continue
+            else:
+                print("***THANK YOU, HOPE YOU ENJOYED THE GAME***")
+                time.sleep(3)
+                break
+    else:
+        print("TIMID MAN")
+main()
